29a29e828f395ed99c553aa725ceb5807da344c1
Budin Kolyandov <budin_kolyandov@abv.bg>
2020-05-03 20:29:57 +0300

    Add project files.

---
+++ ArdesCrawler/ArdesBgDataGatherer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+
+namespace ArdesCrawler
+{
+    public class ArdesBgDataGatherer
+    {
+        public async Task<IEnumerable<RawProduct>> GatherData(string name, int pages)
+        {
+            var products = new List<RawProduct>();
+            var productUrls = new List<string>();
+            var parser = new HtmlParser();
+            var client = new HttpClient();
+
+            for (int page = pages; page >= 1; page--)
+            {
+                Console.Write($"{page} => ");
+
+                var url = $"https://ardes.bg/komponenti/{name}/page/{page}";
+                string htmlContent = null;
+                for (var i = 0; i < 10; i++)
+                {
+                    try
+                    {
+                        var response = await client.GetAsync(url);
+                        htmlContent = await response.Content.ReadAsStringAsync();
+                        break;
+                    }
+                    catch
+                    {
+                        Console.Write('!');
+                        Thread.Sleep(500);
+                    }
+                }
+
+                if (string.IsNullOrWhiteSpace(htmlContent))
+                {
+                    break;
+                }
+
+                var document = await parser.ParseDocumentAsync(htmlContent);
+
+                var elements = document.GetElementsByClassName("product clearfix");
+
+                if (elements.Length == 0)
+                {
+                    break;
+                }
+
+                foreach (var element in elements)
+                {
+                    string ardesUrl = null;
+                    var options = element.InnerHtml.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var option in options)
+                    {
+                        if (option.Contains("href="))
+                        {
+                            // "/product/2tb-adata-hv620s-ahv620s-2tu3-cbk-103788\">\n"
+                            var productUrlUntrimmed = option.Substring(option.IndexOf('/'));
+                            var productUrl = productUrlUntrimmed.Substring(0, productUrlUntrimmed.Length - 3);
+                            ardesUrl = "https://ardes.bg" + productUrl;
+                            productUrls.Add(ardesUrl);
+                        }
+                    }
+                }
+            }
+            foreach (var url in productUrls)
+            {
+                var response = await client.GetAsync(url);
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                var document = await parser.ParseDocumentAsync(htmlContent);
+                var productName = document.QuerySelector(".product-title")?.TextContent.Trim();
+                if (productName.Contains('-'))
+                {
+                    productName = productName.Substring(0, productName.IndexOf('-'));
+                }
+                var imgUrlInnerHtml = document.QuerySelector(".slide-item").InnerHtml;
+                var imgurlFirstPart = imgUrlInnerHtml.Substring(imgUrlInnerHtml.IndexOf("/")).Trim();
+                var imgUrl = "https://ardes.bg" + imgurlFirstPart.Substring(0, imgurlFirstPart.IndexOf('\"'));
+
+                var elements = document.GetElementsByClassName("tech-specs-list");
+                if (elements.Length == 0)
+                {
+                    break;
+                }
+                foreach (var element in elements)
+                {
+                    string capacity = null;
+                    string interfaceType = null;
+                    string type = null;
+                    var results = element.InnerHtml.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+                    var typeRaw = results[1];
+                    if (typeRaw.Contains('<'))
+                    {
+                        var typeFirst = typeRaw.Substring(0, typeRaw.LastIndexOf('<'));
+                        if (typeFirst.Contains('>'))
+                        {
+                            type = typeFirst.Substring(typeFirst.LastIndexOf('>') + 2);
+                        }
+                    }
+                    foreach (var result in results)
+                    {
+                        if (result.Contains("Капацитет") && !results.Contains("-"))
+                        {
+                            var capacityRaw = result;
+                            var capacityFirst = capacityRaw.Substring(0, capacityRaw.LastIndexOf('<'));
+                            capacity = capacityFirst.Substring(capacityFirst.LastIndexOf('>') + 2);
+                        }
+                        else if(result.Contains("Интерфейс"))
+                        {
+                            var interfaceRaw = result;
+                            var interfaceFirst = interfaceRaw.Substring(0, interfaceRaw.LastIndexOf('<'));
+                            interfaceType = interfaceFirst.Substring(interfaceFirst.LastIndexOf('>') + 2);
+                        }
+                    }
+
+                    var product = new RawProduct
+                    {
+                        Name = productName,
+                        ImgUrl = imgUrl,
+                        Type = type,
+                        Capacity = capacity,
+                        Interface = interfaceType,
+                    };
+                    products.Add(product);
+                }
+            }
+            Console.WriteLine($"OK. {products.Count} total product(s).");
+
+            return products;
+        }
+    }
+
+    public class RawProduct
+    {
+        public string ImgUrl { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Capacity { get; set; }
+
+        public string Interface { get; set; }
+    }
+}
+
---
+++ ArdesCrawler/Program.cs
@@ -0,0 +1,356 @@
+﻿namespace ArdesCrawler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using DocumentFormat.OpenXml;
+    using DocumentFormat.OpenXml.Packaging;
+    using DocumentFormat.OpenXml.Spreadsheet;
+    using Newtonsoft.Json;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hardDisksResult = new ArdesBgDataGatherer().GatherData("tvardi-diskove", 54).GetAwaiter().GetResult();
+            var processorsResult = new ArdesBgDataGatherer().GatherData("protsesori", 6).GetAwaiter().GetResult();
+            var motherboardsResult = new ArdesBgDataGatherer().GatherData("danni-platki", 20).GetAwaiter().GetResult();
+            var coolersResult = new ArdesBgDataGatherer().GatherData("ohladiteli", 16).GetAwaiter().GetResult();
+            var videoCardsResult = new ArdesBgDataGatherer().GatherData("video-karti", 21).GetAwaiter().GetResult();
+            var memoriesResult = new ArdesBgDataGatherer().GatherData("pamet", 21).GetAwaiter().GetResult();
+            var powerSupliesResult = new ArdesBgDataGatherer().GatherData("zahranvane", 16).GetAwaiter().GetResult();
+            var casesResult = new ArdesBgDataGatherer().GatherData("kompyutarni-kutii", 22).GetAwaiter().GetResult();
+
+            using (var spreadsheetDocument = SpreadsheetDocument.Create(@"C:\Users\Budin\Desktop\Crawlers\ArdesCrawler\ArdesCrawler\Data\ArdesData.xls", SpreadsheetDocumentType.Workbook))
+            {
+                WorkbookPart workbookPart;
+                WorksheetPart worksheetPart;
+                SheetData sheetData;
+                Sheets sheets;
+                CreateTable(spreadsheetDocument, out workbookPart, out worksheetPart, out sheetData, out sheets);
+                DataTable hardDiscsTable = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(hardDisksResult), (typeof(DataTable)));
+                AddHardDrivesToTable(hardDiscsTable, spreadsheetDocument, worksheetPart, sheetData, sheets);
+
+                DataTable procesorsTable = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(processorsResult), (typeof(DataTable)));
+                AddProcessorsToTable(procesorsTable, spreadsheetDocument, worksheetPart, sheetData, sheets);
+
+                DataTable motherboardsTable = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(motherboardsResult), (typeof(DataTable)));
+                AddMotherboardsToTable(motherboardsTable, spreadsheetDocument, worksheetPart, sheetData, sheets);
+
+                DataTable coolersTable = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(coolersResult), (typeof(DataTable)));
+                AddCoolersToTable(coolersTable, spreadsheetDocument, worksheetPart, sheetData, sheets);
+
+                DataTable videoCardsTable = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(videoCardsResult), (typeof(DataTable)));
+                AddVideoCardsToTable(videoCardsTable, spreadsheetDocument, worksheetPart, sheetData, sheets);
+
+                DataTable memoriesTable = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(memoriesResult), (typeof(DataTable)));
+                AddMemoriesToTable(memoriesTable, spreadsheetDocument, worksheetPart, sheetData, sheets);
+
+                DataTable powerSupliesTable = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(powerSupliesResult), (typeof(DataTable)));
+                AddPowerSupliesToTable(powerSupliesTable, spreadsheetDocument, worksheetPart, sheetData, sheets);
+
+                DataTable casesTable = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(casesResult), (typeof(DataTable)));
+                AddCasesToTable(casesTable, spreadsheetDocument, worksheetPart, sheetData, sheets);
+
+                workbookPart.Workbook.Save();
+
+                // Close the document.
+                spreadsheetDocument.Close();
+
+                Console.WriteLine(@"Excel file created , you can find the file C:\Users\Budin\Desktop\Crawlers\ArdesCrawler\ArdesCrawler\Data\ArdesData.xls");
+            }
+        }
+
+        private static void AddCasesToTable(DataTable casesTable, SpreadsheetDocument spreadsheetDocument, WorksheetPart worksheetPart, SheetData sheetData, Sheets sheets)
+        {
+            Sheet casesSheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 8,
+                Name = "Cases"
+            };
+            sheets.Append(casesSheet);
+
+            Row headerRow = new Row();
+            var columns = new List<string>();
+            foreach (DataColumn column in casesTable.Columns)
+            {
+                columns.Add(column.ColumnName);
+                Cell cell = new Cell();
+                cell.DataType = CellValues.String;
+                cell.CellValue = new CellValue(column.ColumnName);
+                headerRow.AppendChild(cell);
+            }
+            sheetData.AppendChild(headerRow);
+            foreach (DataRow dsrow in casesTable.Rows)
+            {
+                Row newRow = new Row();
+                foreach (var col in columns)
+                {
+                    Cell cell = new Cell();
+                    cell.DataType = CellValues.String;
+                    cell.CellValue = new CellValue(dsrow[col].ToString());
+                    newRow.AppendChild(cell);
+                }
+                sheetData.AppendChild(newRow);
+            }
+        }
+
+        private static void AddPowerSupliesToTable(DataTable powerSupliesTable, SpreadsheetDocument spreadsheetDocument, WorksheetPart worksheetPart, SheetData sheetData, Sheets sheets)
+        {
+            Sheet powerSupliesSheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 7,
+                Name = "PowerSuplies"
+            };
+            sheets.Append(powerSupliesSheet);
+
+            Row headerRow = new Row();
+            var columns = new List<string>();
+            foreach (DataColumn column in powerSupliesTable.Columns)
+            {
+                columns.Add(column.ColumnName);
+                Cell cell = new Cell();
+                cell.DataType = CellValues.String;
+                cell.CellValue = new CellValue(column.ColumnName);
+                headerRow.AppendChild(cell);
+            }
+            sheetData.AppendChild(headerRow);
+            foreach (DataRow dsrow in powerSupliesTable.Rows)
+            {
+                Row newRow = new Row();
+                foreach (var col in columns)
+                {
+                    Cell cell = new Cell();
+                    cell.DataType = CellValues.String;
+                    cell.CellValue = new CellValue(dsrow[col].ToString());
+                    newRow.AppendChild(cell);
+                }
+                sheetData.AppendChild(newRow);
+            }
+        }
+
+        private static void AddMemoriesToTable(DataTable memoriesTable, SpreadsheetDocument spreadsheetDocument, WorksheetPart worksheetPart, SheetData sheetData, Sheets sheets)
+        {
+            Sheet memoriesSheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 6,
+                Name = "Memories"
+            };
+            sheets.Append(memoriesSheet);
+
+            Row headerRow = new Row();
+            var columns = new List<string>();
+            foreach (DataColumn column in memoriesTable.Columns)
+            {
+                columns.Add(column.ColumnName);
+                Cell cell = new Cell();
+                cell.DataType = CellValues.String;
+                cell.CellValue = new CellValue(column.ColumnName);
+                headerRow.AppendChild(cell);
+            }
+            sheetData.AppendChild(headerRow);
+            foreach (DataRow dsrow in memoriesTable.Rows)
+            {
+                Row newRow = new Row();
+                foreach (var col in columns)
+                {
+                    Cell cell = new Cell();
+                    cell.DataType = CellValues.String;
+                    cell.CellValue = new CellValue(dsrow[col].ToString());
+                    newRow.AppendChild(cell);
+                }
+                sheetData.AppendChild(newRow);
+            }
+        }
+
+        private static void AddVideoCardsToTable(DataTable videoCardsTable, SpreadsheetDocument spreadsheetDocument, WorksheetPart worksheetPart, SheetData sheetData, Sheets sheets)
+        {
+            Sheet videoCardsSheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 5,
+                Name = "VideoCards"
+            };
+            sheets.Append(videoCardsSheet);
+
+            Row headerRow = new Row();
+            var columns = new List<string>();
+            foreach (DataColumn column in videoCardsTable.Columns)
+            {
+                columns.Add(column.ColumnName);
+                Cell cell = new Cell();
+                cell.DataType = CellValues.String;
+                cell.CellValue = new CellValue(column.ColumnName);
+                headerRow.AppendChild(cell);
+            }
+            sheetData.AppendChild(headerRow);
+            foreach (DataRow dsrow in videoCardsTable.Rows)
+            {
+                Row newRow = new Row();
+                foreach (var col in columns)
+                {
+                    Cell cell = new Cell();
+                    cell.DataType = CellValues.String;
+                    cell.CellValue = new CellValue(dsrow[col].ToString());
+                    newRow.AppendChild(cell);
+                }
+                sheetData.AppendChild(newRow);
+            }
+        }
+
+        private static void AddCoolersToTable(DataTable coolersTable, SpreadsheetDocument spreadsheetDocument, WorksheetPart worksheetPart, SheetData sheetData, Sheets sheets)
+        {
+            Sheet coolersSheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 4,
+                Name = "Coolers"
+            };
+            sheets.Append(coolersSheet);
+
+            Row headerRow = new Row();
+            var columns = new List<string>();
+            foreach (DataColumn column in coolersTable.Columns)
+            {
+                columns.Add(column.ColumnName);
+                Cell cell = new Cell();
+                cell.DataType = CellValues.String;
+                cell.CellValue = new CellValue(column.ColumnName);
+                headerRow.AppendChild(cell);
+            }
+            sheetData.AppendChild(headerRow);
+            foreach (DataRow dsrow in coolersTable.Rows)
+            {
+                Row newRow = new Row();
+                foreach (var col in columns)
+                {
+                    Cell cell = new Cell();
+                    cell.DataType = CellValues.String;
+                    cell.CellValue = new CellValue(dsrow[col].ToString());
+                    newRow.AppendChild(cell);
+                }
+                sheetData.AppendChild(newRow);
+            }
+        }
+
+        private static void AddMotherboardsToTable(DataTable motherboardsTable, SpreadsheetDocument spreadsheetDocument, WorksheetPart worksheetPart, SheetData sheetData, Sheets sheets)
+        {
+            Sheet motherboardsSheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 3,
+                Name = "Motherboards"
+            };
+            sheets.Append(motherboardsSheet);
+
+            Row headerRow = new Row();
+            var columns = new List<string>();
+            foreach (DataColumn column in motherboardsTable.Columns)
+            {
+                columns.Add(column.ColumnName);
+                Cell cell = new Cell();
+                cell.DataType = CellValues.String;
+                cell.CellValue = new CellValue(column.ColumnName);
+                headerRow.AppendChild(cell);
+            }
+            sheetData.AppendChild(headerRow);
+            foreach (DataRow dsrow in motherboardsTable.Rows)
+            {
+                Row newRow = new Row();
+                foreach (var col in columns)
+                {
+                    Cell cell = new Cell();
+                    cell.DataType = CellValues.String;
+                    cell.CellValue = new CellValue(dsrow[col].ToString());
+                    newRow.AppendChild(cell);
+                }
+                sheetData.AppendChild(newRow);
+            }
+        }
+
+        private static void AddProcessorsToTable(DataTable procesorsTable, SpreadsheetDocument spreadsheetDocument, WorksheetPart worksheetPart, SheetData sheetData, Sheets sheets)
+        {
+            Sheet processorsSheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 2,
+                Name = "Processors"
+            };
+            sheets.Append(processorsSheet);
+
+            Row headerRow = new Row();
+            var columns = new List<string>();
+            foreach (DataColumn column in procesorsTable.Columns)
+            {
+                columns.Add(column.ColumnName);
+                Cell cell = new Cell();
+                cell.DataType = CellValues.String;
+                cell.CellValue = new CellValue(column.ColumnName);
+                headerRow.AppendChild(cell);
+            }
+            sheetData.AppendChild(headerRow);
+            foreach (DataRow dsrow in procesorsTable.Rows)
+            {
+                Row newRow = new Row();
+                foreach (var col in columns)
+                {
+                    Cell cell = new Cell();
+                    cell.DataType = CellValues.String;
+                    cell.CellValue = new CellValue(dsrow[col].ToString());
+                    newRow.AppendChild(cell);
+                }
+                sheetData.AppendChild(newRow);
+            }
+        }
+
+        private static void AddHardDrivesToTable(DataTable table, SpreadsheetDocument spreadsheetDocument, WorksheetPart worksheetPart, SheetData sheetData, Sheets sheets)
+        {
+            Sheet hardDrivesSheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = "HardDrives"
+            };
+            sheets.Append(hardDrivesSheet);
+            Row headerRow = new Row();
+            var columns = new List<string>();
+            foreach (DataColumn column in table.Columns)
+            {
+                columns.Add(column.ColumnName);
+                Cell cell = new Cell();
+                cell.DataType = CellValues.String;
+                cell.CellValue = new CellValue(column.ColumnName);
+                headerRow.AppendChild(cell);
+            }
+            sheetData.AppendChild(headerRow);
+            foreach (DataRow dsrow in table.Rows)
+            {
+                Row newRow = new Row();
+                foreach (var col in columns)
+                {
+                    Cell cell = new Cell();
+                    cell.DataType = CellValues.String;
+                    cell.CellValue = new CellValue(dsrow[col].ToString());
+                    newRow.AppendChild(cell);
+                }
+
+                sheetData.AppendChild(newRow);
+            }
+        }
+
+        private static void CreateTable(SpreadsheetDocument spreadsheetDocument, out WorkbookPart workbookPart, out WorksheetPart worksheetPart, out SheetData sheetData, out Sheets sheets)
+        {
+            workbookPart = spreadsheetDocument.AddWorkbookPart();
+            workbookPart.Workbook = new Workbook();
+
+            worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+            sheetData = new SheetData();
+            worksheetPart.Worksheet = new Worksheet(sheetData);
+
+            sheets = workbookPart.Workbook.AppendChild(new Sheets());
+        }
+    }
+}
