5ae6dadf470befc4215f2a0e5726494440b8fb8a
Budin Kolyandov <budin_kolyandov@abv.bg>
2020-05-25 10:38:18 +0300

    Added the byte array to the items. Removed unnecessary parts.

--- PcPartsPickerCrawler/Program.cs
+++ PcPartsPickerCrawler/Program.cs
@@ -24,7 +24,7 @@ namespace NewEggPartsCrawler
     {
         static void Main(string[] args)
         {
-            // IEnumerable<Cpu> cpuResult;
+            IEnumerable<Cpu> cpuResult;
             // IEnumerable<Memory> memoryResult;
             // IEnumerable<Motherboard> motherboardResult;
             // IEnumerable<VideoCard> videoCardResult;
@@ -35,6 +35,13 @@ namespace NewEggPartsCrawler
             // IEnumerable<HardDiskDrive> hardDrivesResult;
             // IEnumerable<SolidStateDrive> solidStateDrivesResult;
 
+            cpuResult = new NewEggCpuGatherer().GatherCpuData().GetAwaiter().GetResult();
+
+            using (var context = new ApplicationDbContext())
+            {
+                context.Cpus.AddRange(cpuResult);
+            }
+
             // GatherData(out cpuResult, out memoryResult, out motherboardResult, out videoCardResult, out caseResult, out airCoolerResult, out waterCoolerResult, out powerSuppliesResult, out hardDrivesResult, out solidStateDrivesResult);
 
             // SaveData(cpuResult, memoryResult, motherboardResult, videoCardResult, caseResult, airCoolerResult, waterCoolerResult, powerSuppliesResult, hardDrivesResult, solidStateDrivesResult);
@@ -42,14 +49,171 @@ namespace NewEggPartsCrawler
             using (var context = new ApplicationDbContext())
             {
                 var cpus = context.Cpus.Where(x => x.Name != " ").ToList();
-                Console.WriteLine(cpus.Count);
+                context.Cpus.RemoveRange(context.Cpus);
+                context.SaveChanges();
+                Console.WriteLine(cpus.Count);                
 
-                var imgArray = TransformImage(cpus[0].ImgUrl).GetAwaiter().GetResult();
+                foreach (var cpu in cpus)
+                {
+                    Console.WriteLine(cpu.Id);
+                    var imgArray = TransformImage(cpu.ImgUrl).GetAwaiter().GetResult();
+                    cpu.Image = imgArray;
+                }
+                context.Cpus.AddRange(cpus);
+                context.SaveChanges();
+            }
 
-                //foreach (var cpu in cpus)
-                //{
-                //    var imgArray = TransformImage(cpu.ImgUrl);
-                //}
+            using (var context = new ApplicationDbContext())
+            {
+                var airCoolers = context.AirCoolers.Where(x => x.Name != " ").ToList();
+                context.AirCoolers.RemoveRange(context.AirCoolers);
+                context.SaveChanges();
+                Console.WriteLine(airCoolers.Count);
+
+                foreach (var cpu in airCoolers)
+                {
+                    Console.WriteLine(cpu.Id);
+                    var imgArray = TransformImage(cpu.ImgUrl).GetAwaiter().GetResult();
+                    cpu.Image = imgArray;
+                }
+                context.AirCoolers.AddRange(airCoolers);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                var cases = context.Cases.Where(x => x.Name != " ").ToList();
+                context.Cases.RemoveRange(context.Cases);
+                context.SaveChanges();
+                Console.WriteLine(cases.Count);
+
+                foreach (var cpu in cases)
+                {
+                    Console.WriteLine(cpu.Id);
+                    var imgArray = TransformImage(cpu.ImgUrl).GetAwaiter().GetResult();
+                    cpu.Image = imgArray;
+                }
+                context.Cases.AddRange(cases);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                var hdds = context.HardDiskDrives.Where(x => x.Name != " ").ToList();
+                context.HardDiskDrives.RemoveRange(context.HardDiskDrives);
+                context.SaveChanges();
+                Console.WriteLine(hdds.Count);
+
+                foreach (var cpu in hdds)
+                {
+                    Console.WriteLine(cpu.Id);
+                    var imgArray = TransformImage(cpu.ImgUrl).GetAwaiter().GetResult();
+                    cpu.Image = imgArray;
+                }
+                context.HardDiskDrives.AddRange(hdds);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                var memories = context.Memories.Where(x => x.Name != " ").ToList();
+                context.Memories.RemoveRange(context.Memories);
+                context.SaveChanges();
+                Console.WriteLine(memories.Count);
+
+                foreach (var cpu in memories)
+                {
+                    Console.WriteLine(cpu.Id);
+                    var imgArray = TransformImage(cpu.ImgUrl).GetAwaiter().GetResult();
+                    cpu.Image = imgArray;
+                }
+                context.Memories.AddRange(memories);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                var motherboards = context.Motherboards.Where(x => x.Name != " ").ToList();
+                context.Motherboards.RemoveRange(context.Motherboards);
+                context.SaveChanges();
+                Console.WriteLine(motherboards.Count);
+
+                foreach (var cpu in motherboards)
+                {
+                    Console.WriteLine(cpu.Id);
+                    var imgArray = TransformImage(cpu.ImgUrl).GetAwaiter().GetResult();
+                    cpu.Image = imgArray;
+                }
+                context.Motherboards.AddRange(motherboards);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                var psus = context.PSUs.Where(x => x.Name != " ").ToList();
+                context.PSUs.RemoveRange(context.PSUs);
+                context.SaveChanges();
+                Console.WriteLine(psus.Count);
+
+                foreach (var cpu in psus)
+                {
+                    Console.WriteLine(cpu.Id);
+                    var imgArray = TransformImage(cpu.ImgUrl).GetAwaiter().GetResult();
+                    cpu.Image = imgArray;
+                }
+                context.PSUs.AddRange(psus);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                var ssds = context.SolidStateDrives.Where(x => x.Name != " ").ToList();
+                context.SolidStateDrives.RemoveRange(context.SolidStateDrives);
+                context.SaveChanges();
+                Console.WriteLine(ssds.Count);
+
+                foreach (var cpu in ssds)
+                {
+                    Console.WriteLine(cpu.Id);
+                    var imgArray = TransformImage(cpu.ImgUrl).GetAwaiter().GetResult();
+                    cpu.Image = imgArray;
+                }
+                context.SolidStateDrives.AddRange(ssds);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                var videoCards = context.VideoCards.Where(x => x.Name != " ").ToList();
+                context.VideoCards.RemoveRange(context.VideoCards);
+                context.SaveChanges();
+                Console.WriteLine(videoCards.Count);
+
+                foreach (var cpu in videoCards)
+                {
+                    Console.WriteLine(cpu.Id);
+                    var imgArray = TransformImage(cpu.ImgUrl).GetAwaiter().GetResult();
+                    cpu.Image = imgArray;
+                }
+                context.VideoCards.AddRange(videoCards);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                var waterCoolers = context.WaterCoolers.Where(x => x.Name != " ").ToList();
+                context.WaterCoolers.RemoveRange(context.WaterCoolers);
+                context.SaveChanges();
+                Console.WriteLine(waterCoolers.Count);
+
+                foreach (var cpu in waterCoolers)
+                {
+                    Console.WriteLine(cpu.Id);
+                    var imgArray = TransformImage(cpu.ImgUrl).GetAwaiter().GetResult();
+                    cpu.Image = imgArray;
+                }
+                context.WaterCoolers.AddRange(waterCoolers);
+                context.SaveChanges();
             }
 
             // CreateAndFillExcelTable(cpuResult, memoryResult);
@@ -70,9 +234,15 @@ namespace NewEggPartsCrawler
                 {
                     using (var source = await response.Content.ReadAsStreamAsync())
                     {
-                        using (var target = File.OpenWrite(localPath))
+                        byte[] buffer = new byte[16 * 1024];
+                        using (MemoryStream ms = new MemoryStream())
                         {
-                            await source.CopyToAsync(target);
+                            int read;
+                            while ((read = source.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                ms.Write(buffer, 0, read);
+                            }
+                            img = ms.ToArray();
                         }
                     }
                 }
